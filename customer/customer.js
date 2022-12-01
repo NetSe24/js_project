0f1d47dae97a019b47c44a483b8df203c48a0915
NetSe24 <netse0962@gmail.com>
2022-12-01 11:32:38 +0700

    [DISPLAY PRODUCT] : display on seller and customer page

--- customer/customer.js
+++ customer/customer.js
@@ -1,60 +1,14 @@
 
-
-
-
 // Data element -------------------------------
 
 let listProducts = [
     {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MgJTfXaBP92Od0vR0IA2in4aV-ElbiNtpQ&usqp=CAU",
-        "price":"10$",
-        "name":"T-shirts"
-        
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQAiKirLD6H5nMkJNe8ZFdMC2Hmtkp9gRk0w&usqp=CAU",
-        "price":"10$",
-        "name":"Jeans"
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7Rz3Xp4zGNAGi8Mq_HGOorz9yDqfUH3ETxQ&usqp=CAU",
-        "price":"10$",
-        "name":"Skirts"
+        "img": "https://www2.pictures.stylebistro.com/gi/Ralph+Lauren+Front+Row+Spring+2013+Mercedes+jXPBVvpuZgjl.jpg",
+        "price": "25$",
+        "name": "Skirt",
     },
 
 ]
-
-
-let card_container = document.querySelector("#card_container");
-
-for (let product of listProducts ){
-    let card = document.createElement("div");
-    card.className = "card";
-    card_container.appendChild(card);
-
-    let img_product = document.createElement("img");
-    img_product.src = product.img;
-
-    card.appendChild(img_product);
-    
-    let product_name = document.createElement("div");
-    product_name.className = "product_name";
-    product_name.textContent = product.name;
-    card.appendChild(product_name);
-
-    let currency = document.createElement("p");
-    currency.className = "currency";
-    currency.textContent = product.price;
-    card.appendChild(currency);
-
-    let btn_add = document.createElement("button");
-    btn_add.className = "btn_add";
-    btn_add.textContent = " Add >> ";
-    card.appendChild(btn_add);
-
-}
-console.log(card_container)
-
 
 function saveProducts() {
     localStorage.setItem("product", JSON.stringify(listProducts));
@@ -66,5 +20,72 @@ function loadProducts() {
         listProducts = storeProduct;
     }
 }
-// saveProducts();
-// loadProducts();
+
+
+let index = 0;
+
+function renderProduct() {
+    loadProducts();
+
+    let card_container = document.querySelector("#card_container");
+
+    for (index = 0; index < listProducts.length; index++) {
+        let newProduct = listProducts[index];
+
+        let card = document.createElement("div");
+        card.className = "card";
+        card_container.appendChild(card);
+
+        let img_product = document.createElement("img");
+        img_product.id = "img_pic_card";
+        img_product.src = newProduct.img;
+        card.appendChild(img_product);
+
+        let product_name = document.createElement("div");
+        product_name.className = "product_name";
+        
+        product_name.textContent = "Name: ";
+        strong_name = document.createElement("strong");
+        strong_name.textContent = newProduct.name;
+        strong_name.style.color = "orange";
+        product_name.appendChild(strong_name);
+        card.appendChild(product_name);
+
+        let currency = document.createElement("p");
+        currency.className = "currency";
+        currency.textContent = "Price: ";
+        strong_currency = document.createElement("strong");
+        strong_currency.style.color = "orange";
+        strong_currency.textContent = newProduct.price;
+        currency.appendChild(strong_currency);
+        card.appendChild(currency);
+
+        let div_button = document.createElement("div");
+        div_button.className = "div_button";
+        let btn_add = document.createElement("button");
+        div_button.appendChild(btn_add);
+        btn_add.className = "btn_add";
+        btn_add.textContent = "  BUY NOW ! ";
+        card.appendChild(div_button);
+
+        let createStar = document.createElement("div");
+        createStar.className = "create_star";
+
+        for (i = 0; i < 5; i++) {
+            
+            let i = document.createElement("i");
+            i.className = "material-icons star";
+            i.textContent = "star";
+            i.style.color = "white";
+            createStar.appendChild(i);
+
+        }
+        card.appendChild(createStar);
+    }
+
+}
+
+
+
+loadProducts();
+renderProduct();
--- seller/seller.js
+++ seller/seller.js
@@ -1,5 +1,5 @@
 let dialog = document.getElementById("dialog");
-
+let table = document.querySelector("table");
 
 
 
@@ -7,116 +7,29 @@ let dialog = document.getElementById("dialog");
 
 let listProducts = [
     {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxs5-Knk_MwvPlbbqd77jLRjYkugotHPoUw&usqp=CAU",
+        "img": "https://cdn-images.buyma.com/imgdata/item/210128/0064123360/322883442/428.jpg",
         "price": "10$",
         "name": "T-Shirt",
+    },
 
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxs5-Knk_MwvPlbbqd77jLRjYkugotHPoUw&usqp=CAU",
-        "price": "10$",
-        "name": "Jeans",
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxs5-Knk_MwvPlbbqd77jLRjYkugotHPoUw&usqp=CAU",
-        "price": "10$",
-        "name": "Jeans",
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxs5-Knk_MwvPlbbqd77jLRjYkugotHPoUw&usqp=CAU",
-        "price": "10$",
-        "name": "Jeans",
-    },
-    {
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNxs5-Knk_MwvPlbbqd77jLRjYkugotHPoUw&usqp=CAU",
-        "price": "10$",
-        "name": "Jeans",
-    },
-    
+
 ]
 
-
-let tbody = document.querySelector("tbody");
-
-
-for (let product of listProducts) {
-    let tr = document.createElement("tr");
-    tr.className = "tr_body";
-    
-    let t_name = document.createElement("td");
-    t_name.textContent = product.name;
-    
-    let t_picture = document.createElement("td");
-    let img_pic = document.createElement("img");
-    img_pic.className = "img_pic";
-    img_pic.src = product.img;
-    t_picture.appendChild(img_pic);
-    
-    let t_currency = document.createElement("td");
-    t_currency.textContent = product.price;
-
-    let t_edit = document.createElement("td");
-    let img_edit = document.createElement("img");
-    img_edit.className = "img-edit";
-    img_edit.src = "../img/edit.svg";
-    t_edit.appendChild(img_edit);
-
-    let t_delete = document.createElement("td");
-    let img_delete = document.createElement("img");
-    img_delete.className = "img-delete";
-    img_delete.src = "../img/trash.png";
-    t_delete.appendChild(img_delete);
-
-
-    tr.appendChild(t_name);
-    tr.appendChild(t_picture);
-    tr.appendChild(t_currency)
-    tr.appendChild(t_edit)
-    tr.appendChild(t_delete);
-
-    tbody.appendChild(tr);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
+// show 
 
 function show(element) {
     element.style.display = "block";
 }
+
+// hide --------------------------------------------
+
 function hide(element) {
     element.style.display = "none";
 }
-
-function createProduct() {
-    show(dialog);
-}
-
-function Cancel() {
-    hide(dialog);
-}
-
-function createCard() {
-    hide(dialog);
-    let img = document.createElement("img");
-}
-
-
-
+//save and restore -------------------------------------------- 
 function saveProducts() {
     localStorage.setItem("product", JSON.stringify(listProducts));
 }
-
 function loadProducts() {
     let storeProduct = JSON.parse(localStorage.getItem("product"));
     if (storeProduct !== null) {
@@ -124,5 +37,120 @@ function loadProducts() {
     }
 }
 
-saveProducts();
+
+let index = 0;
+
+function renderProduct() {
+
+    let tbody = document.querySelector("#tbody");
+    if (tbody !== null) {
+        tbody.remove();
+    }
+
+    newTbody = document.createElement("tbody");
+    newTbody.id = "tbody";
+
+
+    for (let index = 0; index < listProducts.length; index++) {
+        let newProduct = listProducts[index];
+
+        let tr = document.createElement("tr");
+        tr.className = "tr_body";
+        tr.dataset.index = index;
+
+        let t_name = document.createElement("td");
+        t_name.textContent = newProduct.name;
+        tr.appendChild(t_name);
+
+
+        let t_picture = document.createElement("td");
+        let img_pic = document.createElement("img");
+        img_pic.className = "img_pic";
+        img_pic.src = newProduct.img;
+        t_picture.appendChild(img_pic);
+        tr.appendChild(t_picture);
+
+        let t_currency = document.createElement("td");
+        t_currency.textContent = newProduct.price;
+        tr.appendChild(t_currency)
+
+        let t_edit = document.createElement("td");
+        let img_edit = document.createElement("img");
+        img_edit.className = "img-edit";
+        img_edit.src = "../img/edit.svg";
+        img_edit.addEventListener("click", editProduct);
+        t_edit.appendChild(img_edit);
+        tr.appendChild(t_edit)
+
+        let t_delete = document.createElement("td");
+        let img_delete = document.createElement("img");
+        img_delete.className = "img-delete";
+        img_delete.src = "../img/trash.png";
+        img_delete.addEventListener("click", removeProduct)
+        t_delete.appendChild(img_delete);
+        tr.appendChild(t_delete);
+
+        newTbody.appendChild(tr);
+    }
+    table.appendChild(newTbody)
+    saveProducts();
+}
+
+
+
+function createProduct() {
+    document.querySelector("#create").textContent = "Create"
+    hide(dialog);
+
+    let newListProduct = {};
+
+    let input_pic = document.querySelector("#input_pic");
+    let input_name = document.querySelector("#input_name");
+    let input_price = document.querySelector("#input_price");
+
+    newListProduct.img = input_pic.value;
+    newListProduct.name = input_name.value;
+    newListProduct.price = input_price.value;
+
+    listProducts.push(newListProduct);
+    input_pic.value = "";
+    input_name.value = "";
+    input_price.value = "";
+
+    saveProducts();
+    renderProduct();
+}
+
+
+function removeProduct(event) {
+    let index = event.target.parentElement.parentElement.dataset.index;
+    listProducts.splice(index, 1);
+
+    saveProducts();
+    renderProduct();
+}
+
+function editProduct(event) {
+    document.querySelector("#create").textContent = "Update"
+    let index = event.target.parentElement.parentElement.dataset.index;
+    console.log(index);
+    document.querySelector("#input_pic").value = listProducts[index].img;
+    document.querySelector("#input_name").value = listProducts[index].name;
+    document.querySelector("#input_price").value = listProducts[index].price;
+    show(dialog);
+    listProducts.splice(index, 1);
+    saveProducts();
+}
+
+function addProduct() {
+    show(dialog);
+
+}
+
+function Cancel() {
+    hide(dialog);
+}
+
+// saveProducts();
 loadProducts();
+renderProduct();
